15242660ce0fa5efa3a332e540f69ddd154fae1f
ianwookim <ianwookim@gmail.com>
2011-04-07 21:58:26 +0000

    implement wZp and wTop for ZpHFull model.

--- src/HEP/Automation/MadGraph/Model/ZpHFull.hs
+++ src/HEP/Automation/MadGraph/Model/ZpHFull.hs
@@ -8,6 +8,7 @@ import Text.StringTemplate
 import Text.StringTemplate.Helpers
 
 import HEP.Automation.MadGraph.Model
+import HEP.Automation.MadGraph.Model.Common
 
 data ZpHFull = ZpHFull
   deriving Show
@@ -27,15 +28,27 @@ instance Model ZpHFull where
                  [ ("mZp" , (printf "%.4e" (mZp p)         :: String))
                  , ("gHtu", (printf "%.4e" (gHtu p/sqrt 2) :: String))
                  , ("gHbd", (printf "%.4e" (gHbd p/sqrt 2) :: String))
-                 , ("wZp" , (printf "%.4e" gammaZp :: String))
+                 , ("wTop", (printf "%.4e" (addGammaTop mtop (mZp p) (gHbd p)) :: String))
+                 , ("wZp" , (printf "%.4e" (gammaZp (mZp p) (gHbd p)) :: String))
                  ]
                  (paramCard4Model ZpHFull)  ) ++ "\n\n\n"
- -- Decay width is not right. 
   briefParamShow p 
     = ("M" ++ show (mZp p)
        ++ "GT" ++ show (gHtu p)  
        ++ "GB" ++ show (gHbd p))
 
--- | this is not good at all.
-gammaZp :: Double 
-gammaZp = 1.49027759
+-- | decay width of Zprime
+gammaZp :: Double       -- ^ mZp
+        -> Double       -- ^ gHbd
+        -> Double 
+gammaZp mzp ghbd = ghbd^(2::Int) / (16.0*pi) * mzp -- 0.5*sqrt2 => 1.49027759
+
+-- | additional decay width of top
+addGammaTop :: Double   -- ^ mtop 
+            -> Double   -- ^ mZp  
+            -> Double   -- ^ gHbd
+            -> Double
+addGammaTop mt mzp ghbd = ghbd^(2::Int) / (64.0*pi)* mt^(3::Int) / (mzp*mzp) 
+                          * ( 1- mzp*mzp / (mt*mt))^(2::Int)
+                          * ( 1+ 2.0 * mzp*mzp / (mt*mt) )
+ 
