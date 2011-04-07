83e7462ea8b14cbe799ed00221d2e932963211d0
ianwookim <ianwookim@gmail.com>
2011-04-07 19:55:57 +0000

    add SMHiggs model in SM.hs

--- src/HEP/Automation/MadGraph/Model/SM.hs
+++ src/HEP/Automation/MadGraph/Model/SM.hs
@@ -3,6 +3,9 @@
 module HEP.Automation.MadGraph.Model.SM where
 
 import HEP.Automation.MadGraph.Model
+import Text.Printf
+import Text.StringTemplate
+import Text.StringTemplate.Helpers
 
 data SM = SM 
         deriving Show
@@ -17,3 +20,21 @@ instance Model SM where
     str <- readFile (tpath ++ "/" ++ paramCard4Model SM ++ ".st" )  
     return str
   briefParamShow  SMParam = "" 
+
+data SMHiggs = SMHiggs 
+        deriving Show
+
+instance Model SMHiggs where
+  data ModelParam SMHiggs = SMHiggsParam { mhiggs :: Double, whiggs :: Double } 
+                     deriving Show 
+  briefShow SMHiggs = "SMHiggs"
+  modelName SMHiggs = "sm"
+  paramCard4Model SMHiggs   = "param_card_smhiggs.dat"
+  paramCardSetup tpath SMHiggs p = do  
+    templates <- directoryGroup tpath 
+    return $ ( renderTemplateGroup
+                 templates
+                 [ ("mhiggs", (printf "%.4e" (mhiggs p) :: String) )
+                 , ("whiggs", (printf "%.4e" (whiggs p) :: String) ) ] 
+                 (paramCard4Model SMHiggs) ) ++ "\n\n\n"
+  briefParamShow  (SMHiggsParam m w)  = "MH" ++ show m ++ "WH" ++ show w 
