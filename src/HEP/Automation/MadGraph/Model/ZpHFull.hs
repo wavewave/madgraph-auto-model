180e501d24c694e91df01213e7b7a55b5db74f51
ianwookim <ianwookim@gmail.com>
2011-04-08 00:04:07 +0000

    bugfix in gammaTop

--- src/HEP/Automation/MadGraph/Model/ZpHFull.hs
+++ src/HEP/Automation/MadGraph/Model/ZpHFull.hs
@@ -28,7 +28,7 @@ instance Model ZpHFull where
                  [ ("mZp" , (printf "%.4e" (mZp p)         :: String))
                  , ("gHtu", (printf "%.4e" (gHtu p/sqrt 2) :: String))
                  , ("gHbd", (printf "%.4e" (gHbd p/sqrt 2) :: String))
-                 , ("wTop", (printf "%.4e" (addGammaTop mtop (mZp p) (gHbd p)) :: String))
+                 , ("wTop", (printf "%.4e" (gammaTop mtop (mZp p) (gHbd p)) :: String))
                  , ("wZp" , (printf "%.4e" (gammaZp (mZp p) (gHbd p)) :: String))
                  ]
                  (paramCard4Model ZpHFull)  ) ++ "\n\n\n"
@@ -44,11 +44,12 @@ gammaZp :: Double       -- ^ mZp
 gammaZp mzp ghbd = ghbd^(2::Int) / (16.0*pi) * mzp -- 0.5*sqrt2 => 1.49027759
 
 -- | additional decay width of top
-addGammaTop :: Double   -- ^ mtop 
+gammaTop :: Double   -- ^ mtop 
             -> Double   -- ^ mZp  
             -> Double   -- ^ gHbd
             -> Double
-addGammaTop mt mzp ghbd = ghbd^(2::Int) / (64.0*pi)* mt^(3::Int) / (mzp*mzp) 
+gammaTop mt mzp ghbd = (ghbd^(2::Int) / (64.0*pi)* mt^(3::Int) / (mzp*mzp) 
                           * ( 1- mzp*mzp / (mt*mt))^(2::Int)
-                          * ( 1+ 2.0 * mzp*mzp / (mt*mt) )
- 
+                          * ( 1+ 2.0 * mzp*mzp / (mt*mt) ))
+                       + smgammatop 
+    where smgammatop = 1.508336
