c52fee84ee8c6d4321f4f7f784d8cf1587f32bfa
ianwookim <ianwookim@gmail.com>
2011-06-29 01:37:32 +0000

    add ZpH to ModelParser

--- src/HEP/Automation/MadGraph/Model/ZpH.hs
+++ src/HEP/Automation/MadGraph/Model/ZpH.hs
@@ -4,6 +4,9 @@ module HEP.Automation.MadGraph.Model.ZpH where
 
 import Text.Printf
 
+import Text.Parsec
+import Control.Monad.Identity
+
 import Text.StringTemplate
 import Text.StringTemplate.Helpers
 
@@ -20,6 +23,9 @@ instance Model ZpH where
                       deriving Show
   briefShow ZpH = "Zp"
   modelName ZpH = "zHorizontal_MG"
+  modelFromString str = case str of 
+                          "zHorizontal_MG" -> Just ZpH
+                          _ -> Nothing
   paramCard4Model ZpH  = "param_card_zHorizontal.dat" 
   paramCardSetup tpath ZpH (ZpHParam m g) = do 
     templates <- directoryGroup tpath 
@@ -30,9 +36,28 @@ instance Model ZpH where
                  , ("widthzp"      , (printf "%.4e" (gammaWpZp m g) :: String)) ]
                  (paramCard4Model ZpH) ) ++ "\n\n\n"
   briefParamShow (ZpHParam m g) = "M"++show m++"G"++show g 
-  
-  
+  interpreteParam str = let r = parse zphparse "" str 
+                        in case r of 
+                          Right param -> param 
+                          Left err -> error (show err)
+
+zphparse :: ParsecT String () Identity (ModelParam ZpH) 
+zphparse = do 
+  char 'M' 
+  massstr <- many1 (oneOf "+-0123456789.")
+  char 'G'
+  gstr <- many1 (oneOf "+-0123456789.")
+  return (ZpHParam (read massstr) (read gstr))
+
+
+
 gammaWpZp :: Double -> Double -> Double            
 gammaWpZp mass coup = 
   let r = mtop^(2 :: Int)/ mass^(2 :: Int)  
   in  coup^(2 :: Int) / (16.0 * pi) *mass*( 1.0 - 1.5 * r + 0.5 * r^(3 :: Int))
+
+
+
+
+
+
--- src/HEP/Automation/MadGraph/ModelParser.hs
+++ src/HEP/Automation/MadGraph/ModelParser.hs
@@ -4,7 +4,7 @@ module HEP.Automation.MadGraph.ModelParser where
 
 import HEP.Automation.MadGraph.Model
 import HEP.Automation.MadGraph.Model.AxiGluon
-
+import HEP.Automation.MadGraph.Model.ZpH
 
 
 data ModelBox = forall a. (Model a) => ModelBox a
@@ -13,5 +13,6 @@ data ModelBox = forall a. (Model a) => ModelBox a
 modelParse :: String -> Maybe ModelBox
 modelParse "Axigluon_AV_MG" = Just (ModelBox AxiGluon)
 modelParse "DummyModel"     = Just (ModelBox DummyModel)
+modelParse "zHorizontal_MG" = Just (ModelBox ZpH)
 modelParse _ = Nothing
 
