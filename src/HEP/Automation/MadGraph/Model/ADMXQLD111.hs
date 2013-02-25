4f6cb2f0a7c74eed059018d6daa79a97531ca597
Ian-Woo Kim <ianwookim@gmail.com>
2013-02-25 11:29:56 -0500

    mv ADMXQLD to ADMXQLD111

--- src/HEP/Automation/MadGraph/Model/ADMXQLD111.hs
+++ src/HEP/Automation/MadGraph/Model/ADMXQLD111.hs
@@ -4,18 +4,18 @@
 -----------------------------------------------------------------------------
 -- |
 -- Module      : HEP.Automation.MadGraph.Model.ADMXQLD
--- Copyright   : (c) 2011, 2012 Ian-Woo Kim
+-- Copyright   : (c) 2012,2013 Ian-Woo Kim
 --
 -- License     : BSD3
 -- Maintainer  : Ian-Woo Kim <ianwookim@gmail.com>
 -- Stability   : experimental
 -- Portability : GHC
 --
--- ADM (asymmetric dark matter) XUDD model
+-- ADM (asymmetric dark matter) XQLD model, 111 generation
 -- 
 -----------------------------------------------------------------------------
 
-module HEP.Automation.MadGraph.Model.ADMXQLD where
+module HEP.Automation.MadGraph.Model.ADMXQLD111 where
 
 import Control.Monad.Identity
 import Data.Typeable
@@ -28,23 +28,23 @@ import Text.StringTemplate.Helpers
 import HEP.Automation.MadGraph.Model
 
 -- | 
-data ADMXQLD = ADMXQLD 
+data ADMXQLD111 = ADMXQLD111 
              deriving (Show, Typeable, Data)
 
-instance Model ADMXQLD where 
-  data ModelParam ADMXQLD = ADMXQLDParam { mgluino :: Double 
+instance Model ADMXQLD111 where 
+  data ModelParam ADMXQLD111 = ADMXQLD111Param { mgluino :: Double 
                                          , msquark :: Double
                                          , mslepton :: Double
                                          , mneut :: Double } 
                           deriving Show 
-  briefShow ADMXQLD = "ADMXQLD"
+  briefShow ADMXQLD111 = "ADMXQLD111"
   madgraphVersion _ = MadGraph5
-  modelName _ = "ADMXQLD"
+  modelName _ = "ADMXQLD111"
   modelFromString str = case str of 
-                          "ADMXQLD" -> Just ADMXQLD 
+                          "ADMXQLD111" -> Just ADMXQLD111 
                           _ -> Nothing 
-  paramCard4Model ADMXQLD = "param_card_ADMXQLD.dat"
-  paramCardSetup tpath ADMXQLD (ADMXQLDParam mg msq msl mn) = do 
+  paramCard4Model ADMXQLD111 = "param_card_ADMXQLD111.dat"
+  paramCardSetup tpath ADMXQLD111 (ADMXQLD111Param mg msq msl mn) = do 
     templates <- directoryGroup tpath 
     return $ ( renderTemplateGroup 
                  templates 
@@ -52,15 +52,15 @@ instance Model ADMXQLD where
                  , ("msquark", (printf "%.4e" msq :: String))
                  , ("mslepton", (printf "%.4e" msl :: String))
                  , ("mneut",   (printf "%.4e" mn :: String)) ]
-                 (paramCard4Model ADMXQLD) ) ++ "\n\n\n"
-  briefParamShow (ADMXQLDParam mg msq msl mn) = "MG"++show mg++"MQ"++show msq ++ "ML" ++ show msl ++ "MN"++show mn
+                 (paramCard4Model ADMXQLD111) ) ++ "\n\n\n"
+  briefParamShow (ADMXQLD111Param mg msq msl mn) = "MG"++show mg++"MQ"++show msq ++ "ML" ++ show msl ++ "MN"++show mn
   interpreteParam str = let r = parse xqldparse "" str 
                         in case r of
                           Right param -> param 
                           Left err -> error (show err)
 
 -- | 
-xqldparse :: ParsecT String () Identity (ModelParam ADMXQLD)
+xqldparse :: ParsecT String () Identity (ModelParam ADMXQLD111)
 xqldparse = do 
   string "MG"
   mgstr <- many1 (oneOf "+-0123456789.")
@@ -70,7 +70,7 @@ xqldparse = do
   mlstr <- many1 (oneOf "+-0123456789.")
   string "MN"
   mnstr <- many1 (oneOf "+-0123456789.") 
-  return (ADMXQLDParam (read mgstr) (read mqstr) (read mlstr) (read mnstr))
+  return (ADMXQLD111Param (read mgstr) (read mqstr) (read mlstr) (read mnstr))
 
 -----------------------------
 -- for type representation 
@@ -78,11 +78,11 @@ xqldparse = do
 
 -- | 
 admxqldTr :: TypeRep 
-admxqldTr = mkTyConApp (mkTyCon "HEP.Automation.MadGraph.Model.ADMXQLD.ADMXQLD") []
+admxqldTr = mkTyConApp (mkTyCon "HEP.Automation.MadGraph.Model.ADMXQLD111.ADMXQLD111") []
 
 -- | 
-instance Typeable (ModelParam ADMXQLD) where
+instance Typeable (ModelParam ADMXQLD111) where
   typeOf _ = mkTyConApp modelParamTc [admxqldTr]
 
 -- | 
-deriving instance Data (ModelParam ADMXQLD)
+deriving instance Data (ModelParam ADMXQLD111)
