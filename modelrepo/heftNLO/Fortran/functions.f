6b4dfbaf5fc3a21f67cf421e84fb0c8a77a3ba28
Ian-Woo Kim <ianwookim@gmail.com>
2015-05-08 18:14:11 +0200

    add heftNLO to model repo

---
+++ modelrepo/heftNLO/Fortran/functions.f
@@ -0,0 +1,35 @@
+
+      DOUBLE COMPLEX FUNCTION FormFactor( HALFS )
+
+      DOUBLE COMPLEX HALFS
+      DOUBLE COMPLEX S
+      DOUBLE COMPLEX mt
+      DOUBLE COMPLEX tau
+      DOUBLE COMPLEX beta
+      REAL PIE
+
+      INCLUDE 'coupl.inc'
+      INCLUDE 'input.inc'
+    
+      PIE=3.14159265358979323846264
+
+      S = 2.0*HALFS
+   
+      mt = MDL_MT
+
+      tau = 4*mt**2 / S
+    
+      IF (REAL(tau) .GT. 1.0) THEN  
+        beta = SQRT(1-tau)
+        BRA = 0.5*(LOG((1+beta)/(1-beta)) - (0D0,1D0)*PI)**2
+      ELSE 
+        BRA = -2.0*(ASIN(1/SQRT(tau)))**2
+      END IF
+         
+      ! FormFactor = - mt / (PIE**2 * S)*(1-0.5 * (1 - tau) * BRA ) 
+
+      WRITE(0,*) tau,BRA,FormFactor
+
+      return 
+      end
+
